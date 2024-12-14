0591e4080d8edbc27051c5eee6d3967e012141f6
Dressyapper <Dressyapper879@gmail.com>
2024-12-14 11:28:49 +1100

    thing

---
+++ js/milestones.js
@@ -0,0 +1,7 @@
+milestones: {
+    0;{
+        requirementDescription: "123 Dressy points"
+        effectDescription: "blah",
+        done() ;return player.w.points.gte(123) }
+    }
+    etc
--- js/upgrades.js
+++ js/upgrades.js
@@ -2,7 +2,7 @@ upgrades: {
     11;{
         description: "Blah",
         cost;new Decimal(100)
-        etc
+    
     }
     etc
 }
