665da854778cf99188e3f91bee7ae04903477218
Dressyapper <Dressyapper879@gmail.com>
2024-12-19 15:47:24 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -13,13 +13,13 @@ addLayer("D", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 12, // Prestige currency exponent
+    exponent: 120, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(2)
+        mult = new Decimal(200)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(0.1)
+        return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
