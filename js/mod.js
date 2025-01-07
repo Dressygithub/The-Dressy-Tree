088926fcacf71ee1395d9d7ae0aa4e4113b89ab2
Dressyapper <Dressyapper879@gmail.com>
2025-01-08 10:38:41 +1100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -18,8 +18,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Too lazy.<br>
-		-`
+		- Too lazy.<br>`
+		
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -42,6 +42,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+	if (hasUpgrade('p', 11)) gain = gain.times(2)
 	return gain
 }
 
