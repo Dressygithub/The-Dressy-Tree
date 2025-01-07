5aefe660514bcdf51df20121409e3a82cd53a3fa
Dressyapper <Dressyapper879@gmail.com>
2025-01-08 10:52:02 +1100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -18,7 +18,7 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Too lazy.<br>`
+		- Not using this<br>`
 		
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
