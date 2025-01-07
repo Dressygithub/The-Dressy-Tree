e3bba06075ebb201cf463cbfa4a7bf8324c1bb9c
Dressyapper <Dressyapper879@gmail.com>
2025-01-07 23:27:41 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -23,5 +23,10 @@ addLayer("T", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
+    ],     upgrades: {
+        11: {
+
+        },
+
+    },
 })
