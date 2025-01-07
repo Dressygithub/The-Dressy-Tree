d262e5b18f44e21de08da9e60c186795c2c38b4b
Dressyapper <Dressyapper879@gmail.com>
2025-01-07 18:24:54 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,19 +1,18 @@
-
-addLayer("D", {
-    name: "Dressy", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("T", {
+    name: "Test", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(1),
+		points: new Decimal(0),
     }},
-    color: "#0046ff",
-    requires: new Decimal(1), // Can be a function that takes requirement increases into account
-    resource: "Dressy points", // Name of prestige currency
+    color: "#4BDC13",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1 , // Prestige currency exponent
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -23,6 +22,7 @@ addLayer("D", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "D", description: "D: Reset for Dressy points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    layerShown(){return true}
 })
