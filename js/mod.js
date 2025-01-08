8920ead8fb31c1d403dc52848faf2bc946a4b9b7
Dressyapper <Dressyapper879@gmail.com>
2025-01-08 23:57:28 +1100

    rfeg

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The Dressy Tree",
 	author: "Dressyapper",
 	pointsName: "Points",
-	modFiles: ["DressyLayer.js", "tree.js", "components.js"],
+	modFiles: ["DressyLayer.js", "tree.js", "components.js","achievements.js"],
 
 	discordName: "",
 	discordLink: "",
