659b39a6ad72d1d26db4a88b6665f7a729585571
Dressyapper <Dressyapper879@gmail.com>
2025-01-09 12:21:27 +1100

    Update testinggrounds.js

--- js/testinggrounds.js
+++ js/testinggrounds.js
@@ -1,14 +1,14 @@
 addLayer("T", {
-    name: "Testing points", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Test place", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
                 points: new Decimal(0),
     }},
     color: "#0055ff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Dressy points", // Name of prestige currency
+    resource: "Test", // Name of prestige currency
     baseResource: "Points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -22,7 +22,7 @@ addLayer("T", {
     },
     row: "side", // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "T", description: "T: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ], infoboxes: {
         lore: {
             title: "foo",
