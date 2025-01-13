5ea342c0efe3c50f9bdcbb48c4c512267345bf87
Dressyapper <Dressyapper879@gmail.com>
2025-01-13 12:37:43 +1100

    Update Super.js

--- js/Super.js
+++ js/Super.js
@@ -3,7 +3,7 @@ addLayer("S", {
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
                 points: new Decimal(0),
     }},
     color: "#00fff0",
@@ -23,5 +23,13 @@ addLayer("S", {
     row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
+    ], challenges: {
+        11: {
+            name: "Ouch",
+            challengeDescription: "description of ouchie",
+            canComplete: function() {return player.points.gte(100)},
+            etc
+        },
+        etc
+    }
 })
