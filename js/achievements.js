0a670a764919ae3772d6fd76b5b5b7e4ad48f5a2
Dressyapper <Dressyapper879@gmail.com>
2025-01-17 23:28:24 +1100

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -14,7 +14,7 @@ addLayer("A", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(pow(player.A.points))
+        mult = new Decimal(gain.pow(player.A.points))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
