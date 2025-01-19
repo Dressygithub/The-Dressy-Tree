3e3b1d5f755f8026337569d3146b26c3732040be
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-01-19 11:19:38 +1100

    Update time.js

--- js/time.js
+++ js/time.js
@@ -5,7 +5,6 @@ addLayer("T", {
     startData() { return {
         unlocked: true,
                 points: new Decimal(0),
-     passiveGeneration()
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
