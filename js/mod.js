a0ab23f2aa4223a99d2c3a6f0340bc003972de6b
Dressyapper <Dressyapper879@gmail.com>
2025-01-20 22:48:51 +1100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (inChallange('S', 11)) gain = gain.pow(0.01)
+	if (inChallenge('S', 11)) gain = gain.pow(0.01)
 	if (hasUpgrade('D', 11)) gain = gain.times(2)
 	if (hasUpgrade('D', 12)) gain = gain.times(2)
 	if (hasUpgrade('D', 13)) gain = gain.times(2)
