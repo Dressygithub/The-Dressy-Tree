595fc2443ba52d5e52e5b7dbc6be69b661bba951
Dressyapper <Dressyapper879@gmail.com>
2025-01-22 23:03:56 +1100

    Update DressyLayer.js

--- js/DressyLayer.js
+++ js/DressyLayer.js
@@ -21,7 +21,7 @@ addLayer("D", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade('S', 31)) mult = mult.times(1.6)
-        if (inChallenge('S', 11)) mult = mult.mult(0.5)
+        if (inChallenge('S', 11)) mult = mult.times(0.5)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
