dacf768dcbe460809451facba9be3b8b53909c72
Dressyapper <Dressyapper879@gmail.com>
2025-01-22 23:59:26 +1100

    Update Super.js

--- js/Super.js
+++ js/Super.js
@@ -135,6 +135,7 @@ addLayer("S", {
             name: "Super",
             challengeDescription: "0.5x dressy points",
             goalDescription: "30000 dressy points",
+            rewardDescription: "2x dressy points and a new layer",
             canComplete: function() {return player.D.points.gte(30000)},
         },
     }
