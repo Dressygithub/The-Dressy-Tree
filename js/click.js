8ecc562fd7b752130b6c988cedcedc602696f953
Dressyapper <Dressyapper879@gmail.com>
2025-01-22 16:34:34 +1100

    click

--- js/click.js
+++ js/click.js
@@ -1,6 +1,6 @@
-addLayer("T", {
-    name: "Time", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("C", {
+    name: "Clickery", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
@@ -8,9 +8,9 @@ addLayer("T", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Time", // Name of prestige currency
-    baseResource: "Time", // Name of resource prestige is based on
-    baseAmount() {return player.T.points}, // Get the current amount of baseResource
+    resource: "Click points", // Name of prestige currency
+    baseResource: "Click points", // Name of resource prestige is based on
+    baseAmount() {return player.C.points}, // Get the current amount of baseResource
     type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -21,7 +21,9 @@ addLayer("T", {
         return new Decimal(1)
     },
     row: "side", // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "t", description: "t: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
 })
+clickables: {
+    11: {
+        display() {return "Blah"},
+    }
+}
