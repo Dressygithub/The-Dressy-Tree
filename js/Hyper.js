35bf19a7d0dc7f72fd2ea9f7e04edcd383586281
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-01-25 00:29:36 +1100

    Create Hyper.js

---
+++ js/Hyper.js
@@ -0,0 +1,31 @@
+addLayer("H", {
+    name: "Hyper", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "H", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+                points: new Decimal(500),
+    layerShown(){
+        let visible = false
+        if {hasUpgrade(return hasUpgrade("S",35)} visible = true
+       return visible
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(500), // Can be a function that takes requirement increases into account
+    resource: "Hyper", // Name of prestige currency
+    baseResource: "Super", // Name of resource prestige is based on
+    baseAmount() {return player.S.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "t: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+})
