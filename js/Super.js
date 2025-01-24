0fceca471d2aefac830424b8dc08e58dbb99a3f6
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-01-25 01:04:59 +1100

    j

--- js/Super.js
+++ js/Super.js
@@ -1,5 +1,5 @@
 addLayer("S", {
-    name: "Super Layer", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Super", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -12,7 +12,7 @@ addLayer("S", {
                  },
     }},
     branches: ["S", "H"], 
-    color: "#00ff00",
+    color: "#00fff3",
     requires: new Decimal(1e3), // Can be a function that takes requirement increases into account
     resource: "Super", // Name of prestige currency
     baseResource: "Dressy points", // Name of resource prestige is based on
