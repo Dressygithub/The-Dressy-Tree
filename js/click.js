5d44c2fc5ba8224d6f18e77c604429e545468550
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-01-25 16:30:52 +1100

    click

--- js/click.js
+++ js/click.js
@@ -13,7 +13,7 @@ addLayer("C", {
     baseResource: "Click", // Name of resource prestige is based on
     baseAmount() {return player.C.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -21,8 +21,5 @@ addLayer("C", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "t", description: "t: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
+    row: 1, // Row the layer is in on the tree (0 is the first row)
 })
