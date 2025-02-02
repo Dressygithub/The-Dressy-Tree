349931714216bbc67b0f2309cc3677a7518615f7
Dressyapper <Dressyapper879@gmail.com>
2025-02-03 10:31:19 +1100

    hi

--- js/money.js
+++ js/money.js
@@ -15,9 +15,9 @@ addLayer("Money", {
     requires: new Decimal(150), // Can be a function that takes requirement increases into account
     resource: "Money", // Name of prestige currency
     baseResource: "Money", // Name of resource prestige is based on
-    baseAmount() {return player.M.points}, // Get the current amount of baseResource
+    baseAmount() {return player.H.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1, // Prestige currency exponent
+    exponent: player.points.times(2), // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
