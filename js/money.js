ce9ca84830f51a785f356908e1f6a09c30fe4550
Dressyapper <Dressyapper879@gmail.com>
2025-02-03 12:03:59 +1100

    afe

--- js/money.js
+++ js/money.js
@@ -1,4 +1,4 @@
-addLayer("Money", {
+addLayer("M", {
     name: "Money", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -13,6 +13,7 @@ addLayer("Money", {
      },
     color: "#048c1b",
     requires: new Decimal(150), // Can be a function that takes requirement increases into account
+    resource: "Money", // Name of prestige currency
     baseAmount() {return player.H.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 10000000000000000000000000000000000000, // Prestige currency exponent
