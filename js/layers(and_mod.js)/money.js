0caeb14d5b42f223b1508e971e38c3cbb40f5e58
Dressyapper <Dressyapper879@gmail.com>
2025-02-07 17:30:30 +1100

    jhi

--- js/layers(and_mod.js)/money.js
+++ js/layers(and_mod.js)/money.js
@@ -1,7 +1,7 @@
 addLayer("M", {
     name: "Money", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
                 points: new Decimal(0),
@@ -22,7 +22,7 @@ addLayer("M", {
         return 0
     },
     color: "#048c1b",
-
+    branches: ["M", "Ma"], 
     resource: "Money", // Name of prestige currency
     baseResource: "Hyper", // Name of resource prestige is based on
     baseAmount() {return player.H.points}, // Get the current amount of baseResourc
