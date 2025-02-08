dfc66833ac364e4b6b4d9f6b342a40dbba387d6b
Dressyapper <Dressyapper879@gmail.com>
2025-02-08 13:42:27 +1100

    afe

--- js/math.js
+++ js/math.js
@@ -26,7 +26,7 @@ addLayer("Ma", {
     baseResource: "Money", // Name of resource prestige is based on
     baseAmount() {return player.M.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.1, // Prestige currency exponent
+    exponent: 2.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
