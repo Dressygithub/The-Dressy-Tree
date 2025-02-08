8b5fe7acbc19ff19aa5b15592e7bf9f05f509008
Dressyapper <Dressyapper879@gmail.com>
2025-02-09 00:21:00 +1100

    ad

--- js/mod.js
+++ js/mod.js
@@ -26,7 +26,8 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
-let clicky = 1
+let clicky = new Decimal(1)
+let Ma_effect = new Decimal(0.1)
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
