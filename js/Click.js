bca166848c7752fd6c8dc112450e530a074a247a
Dressyapper <Dressyapper879@gmail.com>
2025-02-09 11:51:12 +1100

    d

--- js/Click.js
+++ js/Click.js
@@ -1,35 +1,30 @@
 addLayer("Mi", {
-    name: "Minigame", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 4, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 4,
     startData() { return {
         unlocked: true,
-                points: new Decimal(0),
+		points: new Decimal(0),
     }},
-    color: "#4BDC13",
-    requires: new Decimal(0), // Can be a function that takes requirement increases into account
-    resource: "Click", // Name of prestige currency
-    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    row: "side", // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "t", description: "t: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ], 
-    clickables: {
-        11: {
-            title: "Click",
-            canClick() {return true},
-            onClick() {return addPoints('C',clicky)}
-        },
+
+    layerShown(){
+        let visible = false
+       return visible
+     },
+    name: "Minigame", // This is optional, only used in a few places, If absent it just uses the layer id.
+    resource: "Minigame points", // Name of prestige currency
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    baseResource: "", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount ganed. static: cost depends on how much you already have
+    exponent: 1.72, // Prestige currency exponent
+    passiveGeneration() {
+        return 1
     },
-    
-    upgrades: {
-        11: {
-            title: "The first click upgrade",
-            description: "Most creative upgrade name ever.",
-            cost: new Decimal(1),
-            effect() {
-                return clicky = new Decimal(2)
-            },
-        },
-    }
+    gainMult() {
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
 })
