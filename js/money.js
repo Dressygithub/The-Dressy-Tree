37ebf2b1f72054dc6f92734bb306db03ef5afd5c
Dressyapper <Dressyapper879@gmail.com>
2025-02-14 20:13:27 +1100

    c

--- js/money.js
+++ js/money.js
@@ -41,7 +41,7 @@ addLayer("M", {
             description: "Generate 1 money per second",
             cost: new Decimal(0),
             onPurchase() {
-                layer.M.moneygain = new Decimal(1)
+                moneygain = 1
             },
         },
         12: {
