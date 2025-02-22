63de3154e8f0ca67de9cf0cafb058f61d064bdd0
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-02-22 16:06:53 +1100

    n

--- js/mod.js
+++ js/mod.js
@@ -26,12 +26,11 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
-var clicky = new Decimal(1)
-var clickformat = new Decimal(clicky)
-var clickmult = new Decimal(1)
-var clickpow = new Decimal(1)
-var Ma_effect = new Decimal(0.1)
-var moneygain = new Decimal(0)
+var clicky = new Decimal()
+var clickmult = new Decimal()
+var clickpow = new Decimal()
+var Ma_effect = new Decimal()
+var moneygain = new Decimal()
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
