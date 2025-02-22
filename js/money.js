67abf58c4cb9ed3b35efc3b147fde6dff63a616f
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-02-22 15:22:27 +1100

    s

--- js/money.js
+++ js/money.js
@@ -21,8 +21,8 @@ addLayer("M", {
     resource: "Money", // Name of prestige currency
     baseResource: "Hyper", // Name of resource prestige is based on
     baseAmount() {return player.H.points}, // Get the current amount of baseResourc
-    //type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    //exponent: 0.0000000000000000000001,
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.0000000000000000000001,
     requires: new Decimal(150),
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(2)
