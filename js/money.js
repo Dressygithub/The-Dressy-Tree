523514419085050e83b5f1801a0c97d41e0d5488
Dressygithub <162633205+Dressygithub@users.noreply.github.com>
2025-02-23 11:00:57 +1100

    xw

--- js/money.js
+++ js/money.js
@@ -47,6 +47,7 @@ addLayer("M", {
             canClick() {return true},
             onClick() {return (addPoints('M',new Decimal(player.H.points).div(2)))
                 (addPoints('H',new Decimal(player.H.points).sub(new Decimal(player.H.points).times(2))))
+                (playaudio('sound.mp3'))
             },
         },
     },
