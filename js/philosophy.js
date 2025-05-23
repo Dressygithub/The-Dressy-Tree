95b14bb6d0e68f0dd9e00afdbedd03bbf9a92753
Dressyapper <Dressyapper879@gmail.com>
2025-05-23 23:56:26 +1000

    Update philosophy.js

--- js/philosophy.js
+++ js/philosophy.js
@@ -14,11 +14,11 @@ addLayer("P", {
         branches: [], 
     color: "#4BDC13",
     requires: new Decimal(1e30), // Can be a function that takes requirement increases into account
-    resource: "", // Name of prestige currency
+    resource: "Philosiphical points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.5, // Prestige currency exponent
+    exponent: 0.01, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -30,4 +30,41 @@ addLayer("P", {
     hotkeys: [
         {key: "t", description: "t: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: { 
+        11: {
+            title: "Ponder",
+            description: "3x points",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "Think",
+            description: "2x dressy point gain",
+            cost: new Decimal(2),
+        },
+        13: {
+            title: "Reflect",
+            description: "1.5x super gain",
+            cost: new Decimal(2),
+        },
+        14: {
+            title: "Revolutionise",
+            description: "1.3x hyper gain",
+            cost: new Decimal(2),
+        },
+        15: {
+            title: "Theory",
+            description: "/1.1 money",
+            cost: new Decimal(2),
+        },
+        16: {
+            title: "Anti",
+            description: "Now you really start to wonder, is there truly an opposite to everything?",
+            cost: new Decimal(0),
+        },
+        21: {
+            title: "Philosophy",
+            description: "Unlock a challenge",
+            cost: new Decimal(0),
+        },
+    }
 })
